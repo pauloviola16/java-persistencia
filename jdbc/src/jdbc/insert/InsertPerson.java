3fbc7a41a41d812fadb22949b771dd1d28369ee7
pauloviola16 <pcviola.dev@gmail.com>
2026-02-19 11:06:13 -0300

    Reorganizando projeto JDBC com estrutura src

---
+++ jdbc/src/jdbc/insert/InsertPerson.java
@@ -0,0 +1,30 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class InsertPerson {
+
+    public static void main(String[] args) throws SQLException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe seu nome");
+        String nome = scanner.nextLine();
+
+        Connection conexao = ConnectionFactory.getConexao();
+
+       String sql = "INSERT INTO PESSOAS (NOME) VALUES (?)";
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        stmt.setString(1, nome);
+        stmt.execute();
+
+        System.out.println("Pessoa incluída com sucesso");
+
+        scanner.close();
+        stmt.close();
+
+    }
+}
